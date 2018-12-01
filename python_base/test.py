88c1d6da03544f247ffadd487729819ba3fa1e63
bing <chen_261624@163.com>
2018-12-01 18:01:19 +0800

    buxi1201 1800

--- python_base/test.py
+++ python_base/test.py
@@ -1,14 +1,36 @@
 #!usr/bin/env python
 # _*_ coding:utf-8 _*_
 
-def fab(max):
-    a, b, n = 0, 1, 1
-    while n < max:
-        a, b = b, a+b
-        n += 1
-        yield b
+# def fab(max):
+#     a, b, n = 0, 1, 1
+#     while n < max:
+#         a, b = b, a+b
+#         n += 1
+#         yield b
+#
+#
+# f = fab(6)
+#
+# print(f.next())
+#
 
 
-f = fab(6)
+us_pw = {'abc': '123', 'dev': '456'}
 
-print(f.next())
+while True:
+    user = input('please input user name:')
+    if user not in us_pw.keys():
+        print('please input the right user name!')
+        continue
+    else:
+        pw = input('please input password:')
+        if pw == False:
+            print('password cannot be null!')
+
+        if pw != None:
+            if us_pw[user] != pw:
+                print('username and pw not match, please input again!')
+                break
+
+            elif us_pw[user] == pw:
+                print('congratuations,username match the pw!')
